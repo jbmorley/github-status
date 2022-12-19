0ca81dc52312ea910f8caf84219ec6816e1258ba
Jason Barrie Morley <jbmorley@mac.com>
2022-12-19 09:08:54 +0000

    feat: Report all project workflows
    This change updates `github-status` to fetch details of all project workflows and report on these. It also introduces a dedicated configuration file which can be used to filter reporting to specific workflows, report on multiple recent builds, and specify branches.

--- github-status.py
+++ github-status.py
@@ -21,30 +21,150 @@
 # SOFTWARE.
 
 import argparse
+import collections
+import datetime
+import itertools
 import os.path
 import pickle
 import pprint
+import sys
 import time
 import webbrowser
 
+import dateutil.parser
+import pytz
 import requests
+import tabulate
 import termcolor
+import yaml
 
 
 SETTINGS_PATH = os.path.expanduser("~/.github-status")
+CONFIGURATION_PATH = os.path.expanduser("~/.github-status-configuration.yaml")
 CLIENT_ID = os.environ["GITHUB_STATUS_CLIENT_ID"]
 
 
+class WorkflowRun(object):
+
+    def __init__(self, details):
+        self._details = details
+
+    @property
+    def repository(self):
+        return self._details['head_repository']['full_name']
+
+    @property
+    def name(self):
+        return self._details['name']
+
+    @property
+    def head_branch(self):
+        return self._details['head_branch']
+
+    @property
+    def conclusion(self):
+        return self._details['conclusion']
+
+    @property
+    def updated_at(self):
+        return dateutil.parser.isoparse(self._details['updated_at'])
+
+    @property
+    def age(self):
+        now = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+        return now - self.updated_at
+
+    @property
+    def age_summary(self):
+        summary = "%dd" % self.age.days
+        if self.age > datetime.timedelta(days=30):
+            return termcolor.colored(summary, "yellow")
+        return summary
+
+    @property
+    def is_success(self):
+        return self.conclusion == 'success'
+
+    @property
+    def is_failure(self):
+        return self.conclusion == 'failure'
+
+    @property
+    def color(self):
+        if self.is_success:
+            return 'green'
+        elif self.is_failure:
+            return 'red'
+        return 'yellow'
+
+    @property
+    def status(self):
+        return termcolor.colored(self.repository, self.color, attrs=["reverse"])
+
+    @property
+    def html_url(self):
+        return self._details["html_url"]
+
+
+class Spinner(object):
+
+    def __init__(self):
+        self._iterator = itertools.cycle(['-', '\\', '|', '/'])
+
+    def update(self):
+        sys.stdout.write(next(self._iterator))
+        sys.stdout.flush()
+        sys.stdout.write('\b')
+
+
+spinner = Spinner()
+
+
+def merge_dicts(*dict_args):
+    result = {}
+    for dictionary in dict_args:
+        result.update(dictionary)
+    return result
+
+
 def get_workflows(token, repository):
+    spinner.update()
     url = f"https://api.github.com/repos/{repository}/actions/workflows"
     return requests.get(url, headers={'Accept': 'application/vnd.github.v3+json',
                                       'Authorization': f"token {token}"}).json()
 
 
-def get_runs(token, repository):
+def get_workflow_runs(token, repository):
+    spinner.update()
     url = f"https://api.github.com/repos/{repository}/actions/runs"
-    return requests.get(url, headers={'Accept': 'application/vnd.github.v3+json',
-                                      'Authorization': f"token {token}"}).json()
+    response = requests.get(url, headers={'Accept': 'application/vnd.github.v3+json',
+                                          'Authorization': f"token {token}"}).json()
+    workflow_runs = [WorkflowRun(workflow_run) for workflow_run in response['workflow_runs']]
+    return workflow_runs
+
+def get_filtered_workflow_runs(token, details):
+
+    counts = collections.defaultdict(int)
+
+    def filter(workflow_run):
+        if "branches" in details and workflow_run.head_branch not in details["branches"]:
+            return False
+        if "workflows" in details and workflow_run.name not in details["workflows"]:
+            return False
+        counts[workflow_run.name] += 1
+        if "limit" in details and counts[workflow_run.name] > details["limit"]:
+            return False
+        return True
+
+    workflows = [workflow_run for workflow_run in get_workflow_runs(token, details["name"]) if filter(workflow_run)]
+
+    # Double-check that we've seen at least one of each requested build.
+    if "workflows" in details:
+        for workflow in details["workflows"]:
+            if workflow not in counts:
+                exit("No runs for workflow '%s' for repository '%s'." % (workflow, details["name"]))
+
+    return workflows
 
 
 def authenticate():
@@ -75,18 +195,9 @@ def authenticate():
     return access_token
 
 
-def color(workflow_run):
-    if workflow_run['conclusion'] is None:
-        return 'yellow'
-    elif workflow_run['conclusion'] == 'success':
-        return 'green'
-    elif workflow_run['conclusion'] == 'failure':
-        return 'red'
-
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('repository', nargs='+')
+    parser.add_argument('repository', nargs='*', default=[])
     options = parser.parse_args()
 
     access_token = None
@@ -99,13 +210,30 @@ def main():
         with open(SETTINGS_PATH, "wb") as fh:
             pickle.dump( {"access_token": access_token}, fh)
 
-    for repository in sorted(options.repository):
-        runs = get_runs(access_token, repository)
-        workflow_runs = [workflow_run for workflow_run in runs['workflow_runs']
-                         if workflow_run['head_branch'] == 'main' or workflow_run['head_branch'] == 'master']
-        workflow_run = workflow_runs[0]
-        conclusion = workflow_run['conclusion']
-        termcolor.cprint("https://github.com/" + repository, color(workflow_run))
+    # Convert the command-line options into the richer configuration format.
+    repositories = [{
+            "name": repository,
+            "branches": ["main", "master"],
+            "limit": 1
+        }
+        for repository in options.repository]
+
+    # Load the configuration if no repositories have been specified.
+    if not repositories:
+        with open(CONFIGURATION_PATH) as fh:
+            configuration = yaml.load(fh, Loader=yaml.SafeLoader)
+            for repository in configuration['repositories']:
+                repository = merge_dicts(configuration["defaults"] if "defaults" in configuration else {}, repository)
+                repositories.append(repository)
+
+    workflow_runs = []
+    for repository_details in repositories:
+        workflow_runs.extend(get_filtered_workflow_runs(access_token, repository_details))
+    workflow_runs.sort(key=lambda x: (x.repository, x.name, x.age))
+
+    rows = [[workflow_run.status, workflow_run.name, workflow_run.age_summary, workflow_run.html_url]
+            for workflow_run in workflow_runs]
+    print(tabulate.tabulate(rows, headers=["Repository", "Workflow", "Age", "URL"]))
 
 
 if __name__ == "__main__":
